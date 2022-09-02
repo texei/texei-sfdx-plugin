d2e41464b8d61f8f702326242bbcde877eea1760
r.siebert <r.siebert@reply.de>
2022-09-02 17:22:17 +0200

    encode with encodeURIComponent

--- src/commands/texei/data/import.ts
+++ src/commands/texei/data/import.ts
@@ -309,7 +309,7 @@ export default class Import extends SfdxCommand {
       this.debug(`DEBUG upserting ${sobjectName} records using external id field '${externalIdField}'`);
 
       records.forEach(record => {
-        record[externalIdField] = encodeURI(record[externalIdField]);
+        record[externalIdField] = encodeURIComponent(record[externalIdField]);
       });
       // max. parallel upsert requests as supported by jsforce (default)
       // https://github.com/jsforce/jsforce/blob/82fcc5284215e95047d0f735dd3037a1aeba5d88/lib/connection.js#L82
